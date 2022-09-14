c44b492fc37eab5bc3715c8e86cda2aee233e61b
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-14 16:16:28 -0700

    script lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,12 @@
  * Return: Always 0 (Success)
  */
 int print_last_digit(int n)
-{	int lastDigit;
-
-	lastDigit = n % 10;
-	_putchar(lastDigit);
-	return (lastDigit);
+{
+	if (n  < 0)
+	{
+		n =  -n;
+	}
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
 }
