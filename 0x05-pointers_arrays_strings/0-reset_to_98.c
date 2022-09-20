3167aa4050b6544dbeb0930a49ac55089fb12b49
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-19 17:28:50 -0700

    script reset_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of param bu ref
+ * @n: pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c: reset the value of parameter with pointer
