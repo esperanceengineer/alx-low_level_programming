1061fb55b6afbbfe6fab8ac036e3fa54919a1106
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-22 18:20:44 -0700

    script strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - compare the string
+ * @a: dest
+ * @b: src
+ * Return: Return the value of comparaison
+ */
+int _strcmp(char *a, char *b)
+{
+	return (strcmp(a, b));
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c: concact two strings
 1-strncat: concat two strings witt the count
 2-strncpy.c : copy the string with the length
+3-strcmp.c: compare two strings
