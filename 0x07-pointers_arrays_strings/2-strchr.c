b45821fcfcc1dafebc528e91c9e5db47a5bdd33a
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-26 15:53:58 -0700

    script strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strchr - searhc the char in a string
+ * @s: string
+ * @b: char to search
+ * Return: the pointer to string
+ */
+char *_strchr(char *s, char b)
+{
+	return (strchr(s, b));
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-memset.c: fills the memeory with a constant byte
 1-memcpy.c :copy the memory area
+2-strchr: search char in a string
