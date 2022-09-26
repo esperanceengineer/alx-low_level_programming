a5d271c8a971d612111389c869132f6859613949
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-26 16:14:46 -0700

    script strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strstr - search for a substring
+ * @s: string
+ * @b: char to search
+ * Return: the pointer to string
+ */
+char *_strstr(char *s, char *b)
+{
+	return (strstr(s, b));
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@
 2-strchr: search char in a string
 3-strspn.c: get the length of a prefix subtring
 4-strpbrk.c: search in a string for any byte
+5-strstr.c: locate the subtring
