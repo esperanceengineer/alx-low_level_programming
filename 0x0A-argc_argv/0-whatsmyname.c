234c513459ee6e9f3da19f919348979400f34dd8
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-09-30 17:51:45 -0700

    whatsmynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point for app
+ * @argc: count of parametres
+ * @argv: array of arguments
+ * Return: always zero
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
