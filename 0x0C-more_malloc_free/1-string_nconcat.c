c612ea421ec740a77499072ecaa21fac42ed692a
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-10-07 19:22:29 -0700

    script string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *string_nconcat - concat th sring
+ *@s1: string to concat
+ *@s2: string to concat
+ *@len: number of char
+ * Return: return the pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int len)
+{
+	char *str;
+	unsigned int i, n, m, k = 0, count;
+
+	n = (s1 == NULL) ? 1 : strlen(s1);
+	m = (s2 == NULL) ? 1 : strlen(s2);
+	str = malloc(sizeof(char) * (n  + m + 1));
+	if (str == NULL)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			str[k] = s1[i];
+			k++;
+		}
+	}
+	if (s2 != NULL)
+	{
+		count = (len >= m) ? m : len;
+		for (i = 0; i < count; i++)
+		{
+			str[k] = s2[i];
+			k++;
+		}
+	}
+	str[k] = '\0';
+	return (str);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,2 @@
 0-malloc_checked.c: check if allocation is created succesfully
+0-string_nconcat.c: contact n char of string S1 to S2
