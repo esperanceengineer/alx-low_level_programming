5ef1256fe4e9ff5155771093af70c270c708b3a8
Espérance Engineer <alladoumndoubaesperance@gmail.com>
2022-10-07 19:48:48 -0700

    Try Fix buf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int len)
 
 	n = (s1 == NULL) ? 1 : strlen(s1);
 	m = (s2 == NULL) ? 1 : strlen(s2);
-	str = malloc(sizeof(char) * (n  + m + 1));
+	str = (char *)malloc(sizeof(char) * (n  + m + 1));
 	if (str == NULL)
 		return (NULL);
 	if (s1 != NULL)
